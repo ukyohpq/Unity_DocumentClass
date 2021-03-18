0f16ac3822a56ff68b1fbeeb59a0c0320fa00802
pengqihe <pengqihe@babeltime.com>
2021-03-18 23:14:04 +0800

    函数重命名 添加测试类

---
+++ Assets/Lua/CustomGame/UI/TestContainer.lua
@@ -0,0 +1,21 @@
+---@type Framework.UI.Prefab
+local super = require("Framework.UI.Prefab")
+
+---@class CustomGame.UI.TestContainer:Framework.UI.Prefab
+---@field m_Doc CustomGame.UI.TestUI
+TestContainer = class("CustomGame.UI.TestContainer", super)
+
+---OnComplete
+---@param evt Framework.event.Event
+function TestContainer:OnComplete(evt)
+end
+
+function TestContainer:GetAssetPath()
+    if IsEditor then
+        return "Assets/prefabs/TestContainer.prefab"
+    else
+        return "2"
+    end
+end
+
+return TestContainer
