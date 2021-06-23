3eccac28a8d4e8022720d0e9ab0df431e0f91658
pengqihe <pengqihe@babeltime.com>
2021-06-23 22:56:21 +0800

    添加active接口

---
+++ Assets/Script/Framework/LuaUI/UIExtends/LuaDisplayObject.cs
@@ -0,0 +1,43 @@
+using System;
+using Framework.core.Components;
+using LuaInterface;
+
+namespace Framework.LuaUI.UIExtends
+{
+    public class LuaDisplayObject
+    {
+        public static void Extend(LuaState ls)
+        {
+            ls.LuaGetGlobal("DisplayObject");
+            if (ls.LuaIsNil(-1))
+            {
+                ls.LuaPop(1);
+                throw new Exception("can not find lua Class: DisplayObject");
+            }
+            ls.LuaPushFunction(SetActiveExtend);
+            ls.LuaSetField(-2, "SetActiveExtend");
+            ls.LuaPop(1);
+        }
+
+        private static int SetActiveExtend(IntPtr L)
+        {
+            try
+            {
+                ToLua.CheckArgsCount(L, 2);
+                LuaDLL.lua_pushvalue(L, -2);
+                LuaDLL.lua_gettable(L, LuaIndexes.LUA_REGISTRYINDEX);
+                var binder = ToLua.ToVarObject(L, -1) as GameObjectLuaBinder;
+                var active = LuaDLL.tolua_toboolean(L, -2);
+                binder.gameObject.SetActive(active);
+                LuaDLL.lua_pop(L, 2);
+            }
+            catch (Exception err)
+            {
+                return LuaDLL.toluaL_exception(L, err);
+            }
+
+            return 0;
+        }
+        
+    }
+}
