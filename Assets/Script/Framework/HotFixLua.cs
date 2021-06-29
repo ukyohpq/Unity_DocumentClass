c000fdc6a647dafbcf524050f13fadc13a3b9563
pengqihe <pengqihe@babeltime.com>
2021-06-29 14:54:54 +0800

    将定时遍历文件修改时间改为使用FileSystemWatcher监控文件变化

--- Assets/Script/Framework/HotFixLua.cs
+++ Assets/Script/Framework/HotFixLua.cs
@@ -1,82 +1,57 @@
-﻿/**
- * Created by ukyohpq.
- * DateTime: 17/12/26 11:50
- */
 using System;
 using System.Collections.Generic;
-using UnityEngine;
-using System.Threading;
+using System.IO;
+using Babeltime.Log;
 using LuaInterface;
+using UnityEngine;
 
 namespace Framework.core
 {
-    public class HotFixLua : MonoBehaviour
+//    用FileSystemWatcher实现的文件变化处理，目前还未完成，需要多线程处理
+    public class HotFixLua:MonoBehaviour
     {
         public string LuaPath;
-        private Thread rluaTread;
-
+        private string luaFullPath;
         private List<string> luaModiQue;
-
-        // Use this for initialization
-        void Start()
+        private void Start()
         {
             luaModiQue = new List<string>();
-            rluaTread = new Thread(checkLuaChange);
-            rluaTread.Start(Application.dataPath + LuaPath);
+            var watcher = new FileSystemWatcher();
+            watcher.BeginInit();
+            watcher.Filter = "*.lua";
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+            watcher.NotifyFilter = NotifyFilters.LastWrite| NotifyFilters.Size;
+            watcher.Path = Application.dataPath + LuaPath;
+            watcher.Changed += new FileSystemEventHandler(OnCfgFileChange);
+            watcher.EndInit();
+            luaFullPath = Application.dataPath + LuaPath;
         }
 
-        private void checkLuaChange(object param)
+        private void OnCfgFileChange(object sender, FileSystemEventArgs e)
         {
-            string luaPath = param.ToString();
-            var luaDirInfo = new System.IO.DirectoryInfo(luaPath);
-            var luaFiles = new Dictionary<string, System.DateTime>();
-            foreach (var file in luaDirInfo.GetFiles("*.lua", System.IO.SearchOption.AllDirectories))
+            var luaPath = luaFullPath;
+            var luaFilePath = e.FullPath;
+            if (luaFilePath.IndexOf("\\") != -1)
             {
-                luaFiles[file.FullName] = file.LastWriteTime;
+                luaFilePath = luaFilePath.Replace(".lua", "");
+                luaFilePath = luaFilePath.Replace("\\", ".");
+                luaPath = luaPath.Replace("/", ".");
+                luaFilePath = luaFilePath.Replace(luaPath, "");
             }
-
-            while (true)
+            else
             {
-                foreach (var file in luaDirInfo.GetFiles("*.lua", System.IO.SearchOption.AllDirectories))
-                {
-                    if (!luaFiles.ContainsKey(file.FullName))
-                    {
-                        luaFiles[file.FullName] = file.LastWriteTime;
-                        lock (luaModiQue)
-                        {
-                            luaModiQue.Add(file.FullName);
-                        }
-                    }
-                    else if (!System.DateTime.Equals(luaFiles[file.FullName], file.LastWriteTime))
-                    {
-                        luaFiles[file.FullName] = file.LastWriteTime;
-                        var luaFilePath = file.FullName;
-                        if (luaFilePath.IndexOf("\\") != -1)
-                        {
-                            luaFilePath = luaFilePath.Replace(".lua", "");
-                            luaFilePath = luaFilePath.Replace("\\", ".");
-                            luaPath = luaPath.Replace("/", ".");
-                            luaFilePath = luaFilePath.Replace(luaPath, "");
-                        }
-                        else
-                        {
-                            luaFilePath = luaFilePath.Replace(luaPath, "");
-                            luaFilePath = luaFilePath.Replace(".lua", "");
-                            luaFilePath = luaFilePath.Replace("/", ".");
-                        }
-
-                        lock (luaModiQue)
-                        {
-                            luaModiQue.Add(luaFilePath);
-                        }
-                    }
-                }
-
-                System.Threading.Thread.Sleep(1000);
+                luaFilePath = luaFilePath.Replace(luaPath, "");
+                luaFilePath = luaFilePath.Replace(".lua", "");
+                luaFilePath = luaFilePath.Replace("/", ".");
+            }
+//            BTLog.Error("hot2 OnCfgFileChange:{0} LuaPath:{1}", luaFilePath, luaPath);
+            lock (luaModiQue)
+            {
+                luaModiQue.Add(luaFilePath);
             }
         }
-
-        // Update is called once per frame
+        
         void Update()
         {
             string luaFilePath = "";
